9121d7ef0474b9359a27ffe14ce3183fcbc0811a
Fahad-Almaani <fahad.almaani.fa@gmail.com>
2026-05-03 14:47:52 +0400

    update creating table SQL commands

--- database-project/SQL/Creating tables.sql
+++ database-project/SQL/Creating tables.sql
@@ -1,43 +1,44 @@
 CREATE TABLE Airport (
 	IATA_code VARCHAR(10) Primary Key,
-	name VARCHAR(100),
-	city VARCHAR(100),
-	country VARCHAR(100)
+	name VARCHAR(100) NOT NULL,
+	city VARCHAR(100) NOT NULL,
+	country VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE Passenger(
 	PID int Primary Key,
-	full_name VARCHAR(100),
-	DOB DATE,
-	email VARCHAR(100),
-	phone VARCHAR(30)
+	full_name VARCHAR(100) NOT NULL,
+	DOB DATE NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	phone VARCHAR(30),
+	nationality VARCHAR(50)
 );
 
 CREATE TABLE Aircraft(
 	Reg_number VARCHAR(20) PRIMARY KEY,
-	seat_capacity INT
+	seat_capacity INT NOT NULL
 		CHECK (seat_capacity > 0),
-	model VARCHAR(100),
-	manfucatruer VARCHAR(100),
+	model VARCHAR(100) NOT NULL,
+	manfucatruer VARCHAR(100) NOT NULL,
 	year_of_manf INT,
 );
 
 CREATE TABLE Crew(
 	crewID INT Primary Key,
-	license_num VARCHAR(50),
-	role VARCHAR(50)
+	license_num VARCHAR(50) NOT NULL,
+	role VARCHAR(50) NOT NULL
 		CHECK (role in ('Pilot', 'Co-Pilot', 'Flight Attendant', 'Engineer')),
-	full_name VARCHAR(100)
+	full_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE Flight (
 	fNumber INT Primary Key,
-	from_airport VARCHAR(10),
+	from_airport VARCHAR(10) NOT NULL,
 	to_airport VARCHAR(10),
 	aircraft_id VARCHAR(20),
-	a_time DATETIME,
-	d_time DATETIME,
-	status VARCHAR(50)
+	a_time DATETIME NOT NULL,
+	d_time DATETIME NOT NULL,
+	status VARCHAR(50) NOT NULL
 		DEFAULT 'Scheduled',
 		CHECK (status IN ('Scheduled', 'Delayed', 'Cancelled', 'Completed')),
 
@@ -63,14 +64,14 @@ CREATE TABLE Flight (
 
 CREATE TABLE Booking (
 	BID INT PRIMARY KEY,
-	class VARCHAR(30)
+	class VARCHAR(30) NOT NULL
 		CHECK (class IN ('Economy', 'Business', 'First')),
-	price DECIMAL(10,2)
+	price DECIMAL(10,2) NOT NULL
 		CHECK (price > 0),
 	passenger_id INT,
 	flight_number INT,
-	seat_number VARCHAR(10),
-	booking_date DATE
+	seat_number VARCHAR(10) NOT NULL,
+	booking_date DATE NOT NULL
 		DEFAULT GETDATE(),
 
 	CONSTRAINT FK_Booking_Flight
